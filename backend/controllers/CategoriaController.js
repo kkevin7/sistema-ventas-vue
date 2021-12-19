0872527f4f432d4fe82fc9129192e0804b74103e
Kevin Martinez <kkevinmartinez7@gmail.com>
2021-12-19 17:55:53 -0600

    search filters

--- backend/controllers/CategoriaController.js
+++ backend/controllers/CategoriaController.js
@@ -1,7 +1,7 @@
 import models from '../models';
 
 export default {
-    add: async(req, res, next) => {
+    add: async (req, res, next) => {
         try {
             const reg = await models.Categoria.create(req.body);
             res.status(200).json(reg);
@@ -12,26 +12,27 @@ export default {
             next(e);
         }
     },
-    query: async (req,res,next) => {
+    query: async (req, res, next) => {
         try {
-            const reg=await models.Categoria.findOne({_id: req.query._id});
-            if (!reg){
+            const reg = await models.Categoria.findOne({ _id: req.query._id });
+            if (!reg) {
                 res.status(404).send({
                     message: 'El registro no existe'
                 });
-            } else{
+            } else {
                 res.status(200).json(reg);
             }
-        } catch(e){
+        } catch (e) {
             res.status(500).send({
-                message:'Ocurrió un error'
+                message: 'Ocurrió un error'
             });
             next(e);
         }
     },
-    list: async(req, res, next) => {
+    list: async (req, res, next) => {
         try {
-            const reg = await models.Categoria.find({});
+            let valor = req.query.valor;
+            const reg = await models.Categoria.find({ $or: [{ 'nombre': new RegExp(valor, 'i') }, { 'descripcion': new RegExp(valor, 'i') }] }, { createAt: 0 }).sort({ 'createAt': -1 });
             res.status(200).json(reg);
         } catch (e) {
             res.status(500).send({
@@ -40,31 +41,31 @@ export default {
             next(e);
         }
     },
-    update: async (req,res,next) => {
+    update: async (req, res, next) => {
         try {
-            const reg = await models.Categoria.findByIdAndUpdate({_id:req.body._id},{nombre:req.body.nombre,descripcion:req.body.descripcion});
+            const reg = await models.Categoria.findByIdAndUpdate({ _id: req.body._id }, { nombre: req.body.nombre, descripcion: req.body.descripcion });
             res.status(200).json(reg);
-        } catch(e){
+        } catch (e) {
             res.status(500).send({
-                message:'Ocurrió un error'
+                message: 'Ocurrió un error'
             });
             next(e);
         }
     },
-    remove: async (req,res,next) => {
+    remove: async (req, res, next) => {
         try {
-            const reg = await models.Categoria.findByIdAndDelete({_id:req.body._id});
+            const reg = await models.Categoria.findByIdAndDelete({ _id: req.body._id });
             res.status(200).json(reg);
-        } catch(e){
+        } catch (e) {
             res.status(500).send({
-                message:'Ocurrió un error'
+                message: 'Ocurrió un error'
             });
             next(e);
         }
     },
-    activate:async(req, res, next) => {
+    activate: async (req, res, next) => {
         try {
-            const reg = await models.Categoria.findByIdAndUpdate({_id: req.body._id}, {estado: 1});
+            const reg = await models.Categoria.findByIdAndUpdate({ _id: req.body._id }, { estado: 1 });
             res.status(200).json(reg);
         } catch (e) {
             res.status(500).send({
@@ -73,9 +74,9 @@ export default {
             next(e);
         }
     },
-    deactivate: async(req, res, next) => {
+    deactivate: async (req, res, next) => {
         try {
-            const reg = await models.Categoria.findByIdAndUpdate({_id: req.body._id}, {estado:  0});
+            const reg = await models.Categoria.findByIdAndUpdate({ _id: req.body._id }, { estado: 0 });
             res.status(200).json(reg);
         } catch (e) {
             res.status(500).send({
