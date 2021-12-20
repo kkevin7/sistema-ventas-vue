0a91dcd49ca2a4d754bd991e3dce7da64efe2a56
Kevin Martinez <kkevinmartinez7@gmail.com>
2021-12-19 22:18:40 -0600

    Persona Model

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,11 @@
 import Categoria from "./Categoria";
 import Articulo from "./Articulo";
 import Usuario from "./Usuario";
+import Persona from "./Persona";
 
 export default {
     Categoria,
     Articulo,
     Usuario,
+    Persona,
 }
