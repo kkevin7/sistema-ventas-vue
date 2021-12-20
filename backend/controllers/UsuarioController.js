a66018f9160625e975cfb7ce7b5b013fb897b6d5
Kevin Martinez <kkevinmartinez7@gmail.com>
2021-12-19 22:13:11 -0600

    jsonwebtoken and auth user with middlewares

--- backend/controllers/UsuarioController.js
+++ backend/controllers/UsuarioController.js
@@ -1,5 +1,6 @@
 import models from '../models';
-import bcrypt from 'bcryptjs/dist/bcrypt';
+import bcrypt from 'bcryptjs';
+import token from '../services/token';
 
 export default {
     add: async (req, res, next) => {
@@ -98,6 +99,31 @@ export default {
         try {
             const reg = await models.Usuario.findByIdAndUpdate({ _id: req.body._id }, { estado: 0 });
             res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
+            });
+            next(e);
+        }
+    },
+    login: async (req, res, next) => {
+        try {
+            let user = await models.Usuario.findOne({email:req.body.email});
+            if(user){
+                let match = await bcrypt.compare(req.body.password, user.password);
+                if(match){
+                    let tokenReturn = await token.encode(user._id);
+                    res.status(200).json({user, tokenReturn });
+                }else{
+                    res.status(404).send({
+                        message: 'Password Incorrecto',
+                    })
+                }
+            }else{
+                res.status(404).send({
+                    message: 'No existe el usuario',
+                });
+            }
         } catch (e) {
             res.status(500).send({
                 message: 'Ocurrio un error'
---
+++ backend/services/token.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import models from '../models';
+
+const PASS_SECRET = 'clavesecretaparagenerartoken';
+
+async function checkToken(token){
+    let __id = null;
+    try{
+        const {_id}= await jwt.decode(token);
+        __id = _id;
+    } catch (e){
+        return false;
+    }
+    const user = await models.Usuario.findOne({_id:__id,estado:1});
+    if (user){
+        const token = jwt.sign({_id:__id}, PASS_SECRET,{expiresIn:'1d'});
+        return {token,rol:user.rol};
+    } else {
+        return false;
+    }
+}
+
+export default {
+    encode: async (_id) => {
+        const token = jwt.sign({_id:_id}, PASS_SECRET, {expiresIn: '1d'});
+        return token;
+    },
+    decode: async(token) => {
+        try {
+            const {_id} = await jwt.verify(token, PASS_SECRET);
+            const user = await models.Usuario.findOne({_id, estado: 1});
+            if(user){
+                return user;
+            }else{
+                return false;
+            }
+        } catch (error) {
+            const newToken = await checkToken(token);
+            return newToken;
+        }
+    }
+}
