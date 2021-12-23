02411c87ecaf2afbd62ea1c4e5321a687bf0e528
Kevin Martinez <kkevinmartinez7@gmail.com>
2021-12-23 15:44:45 -0600

    Ingreso Controller and routes

---
+++ backend/controllers/IngresoController.js
@@ -0,0 +1,126 @@
+import models from '../models';
+
+async function aumentarStock(id_acticulo, cantidad){
+    let {stock} = await models.Articulo.findOne({_id: id_acticulo});
+    let nStock = parseInt(stock)+parseIntI(cantidad);
+    const reg = await models.Articulo.findByIdAndUpdate({_id: id_acticulo}, {stock: nStock});
+}
+async function disminuirStock(id_acticulo, cantidad){
+    let {stock} = await models.Articulo.findOne({_id: id_acticulo});
+    let nStock = parseInt(stock)-parseIntI(cantidad);
+    const reg = await models.Articulo.findByIdAndUpdate({_id: id_acticulo}, {stock: nStock});
+}
+
+export default {
+    add: async (req, res, next) => {
+        try {
+            const reg = await models.Ingreso.create(req.body);
+            //Actualizar Stock
+            let detalles = req.body.detalles;
+            detalles.map(function(x){
+                aumentarStock(x._id, x.cantidad);
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
+            });
+            next(e);
+        }
+    },
+    query: async (req, res, next) => {
+        try {
+            const reg = await models.Ingreso.findOne({ _id: req.query._id })
+            .populate('usuario', {nombre: 1})
+            .pupulate('persona', {nombre: 1});
+            if (!reg) {
+                res.status(404).send({
+                    message: 'El registro no existe'
+                });
+            } else {
+                res.status(200).json(reg);
+            }
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    list: async (req, res, next) => {
+        try {
+            let valor = req.query.valor;
+            const reg = await models.Categoria
+            .find({ $or: [{ 'num_comprobante': new RegExp(valor, 'i') }, { 'serie_comprobantre': new RegExp(valor, 'i') }] }, { createAt: 0 })
+            .populate('usuario', {nombre: 1})
+            .pupulate('persona', {nombre: 1})
+            .sort({ 'createAt': -1 });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
+            });
+            next(e);
+        }
+    },
+    /*
+    update: async (req, res, next) => {
+        try {
+            const reg = await models.Ingreso.findByIdAndUpdate({ _id: req.body._id }, 
+                { 
+                    nombre: req.body.nombre,
+                    descripcion: req.body.descripcion,
+                });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    remove: async (req, res, next) => {
+        try {
+            const reg = await models.Ingreso.findByIdAndDelete({ _id: req.body._id });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    */
+    activate: async (req, res, next) => {
+        try {
+            const reg = await models.Ingreso.findByIdAndUpdate({ _id: req.body._id }, { estado: 1 });
+            //Actualizar Stock
+            let detalles = reg.deatalles;
+            detalles.map(function(x){
+                aumentarStock(x._id, x.cantidad);
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
+            });
+            next(e);
+        }
+    },
+    deactivate: async (req, res, next) => {
+        try {
+            const reg = await models.Ingreso.findByIdAndUpdate({ _id: req.body._id }, { estado: 0 });
+            //Actualizar Stock
+            let detalles = reg.deatalles;
+            detalles.map(function(x){
+                disminuirStock(x._id, x.cantidad);
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
+            });
+            next(e);
+        }
+    },
+}
