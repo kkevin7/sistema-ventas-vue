3eba31eb1600f0ee80c8109c5b5fccc2d402938e
Kevin Martinez <kkevinmartinez7@gmail.com>
2021-12-23 14:05:11 -0600

    Ingreso Model

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,10 +2,12 @@ import Categoria from "./Categoria";
 import Articulo from "./Articulo";
 import Usuario from "./Usuario";
 import Persona from "./Persona";
+import Ingreso from "./Ingreso";
 
 export default {
     Categoria,
     Articulo,
     Usuario,
     Persona,
+    Ingreso,
 }
